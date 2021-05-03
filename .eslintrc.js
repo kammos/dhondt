fe23b8c2bda01dacc93c3e009472a813f4bd7886
2bad <kamil.mosior@onet.pl>
2021-05-03 23:07:40 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-hooks"
+    ],
+    "rules": {
+        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+        "react-hooks/exhaustive-deps": "error",
+        "react/react-in-jsx-scope": 0,
+        "react/prop-types": 0,
+        "react/no-unknown-property": [1, { ignore: ["class"] }]
+    }
+};
