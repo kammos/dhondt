27bd44132df65bb4f1dcb503ae7b0cfeb5436150
brainlessbadger <kamil.mosior@onet.pl>
2022-02-05 22:52:27 +0100

    fix NaN issue for inputs of type number

--- src/input/index.js
+++ src/input/index.js
@@ -3,9 +3,16 @@ import camelCase from "lodash/camelCase";
 import startCase from "lodash/startCase";
 import s from "./styles.scss";
 
-export const Input = ({name, type, className, style, ...rest}) => {
+const normalizeForNumber = value => 
+    Number.isInteger(value) ? value : '';
+
+export const Input = ({name, type, value, className, style, ...rest}) => {
+    const inputValue = type === "number" 
+        ? normalizeForNumber(value)
+        : value;
+
     return <label className={cnb(s.label, s[type])}>
         <span className={s.labelText}>{startCase(camelCase(name))}:</span>
-        <input name={name} type={type} className={s.input} {...rest} />
+        <input name={name} type={type} className={s.input} value={inputValue} {...rest} />
     </label>
 }
