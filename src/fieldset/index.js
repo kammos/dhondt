36f1d15532a23fea9ca516d97cc0d770f97f44cf
brainlessbadger <kamil.mosior@onet.pl>
2022-02-05 03:25:42 +0100

    typo

--- src/fieldset/index.js
+++ src/fieldset/index.js
@@ -2,7 +2,7 @@ import s from "./styles.scss";
 
 export const Fieldset = ({color = 0, children, className, style, ...rest}) => {
     const hueAngle = (color * 110) % 360;
-    const borderColor = `hsl(${hueAngle}, 800%, 40%)`;
+    const borderColor = `hsl(${hueAngle}, 80%, 40%)`;
 
     return <fieldset className={s.fieldset} style={{borderColor}} {...rest} >
         {children}
