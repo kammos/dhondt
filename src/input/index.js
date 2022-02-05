141bca5830013c3945b68a932467e7aaf6b926c8
brainlessbadger <kamil.mosior@onet.pl>
2022-02-05 15:44:09 +0100

    correct className wawrning in input

--- src/input/index.js
+++ src/input/index.js
@@ -5,7 +5,7 @@ import s from "./styles.scss";
 
 export const Input = ({name, type, className, style, ...rest}) => {
     return <label className={cnb(s.label, s[type])}>
-        <span class={s.labelText}>{startCase(camelCase(name))}:</span>
+        <span className={s.labelText}>{startCase(camelCase(name))}:</span>
         <input name={name} type={type} className={s.input} {...rest} />
     </label>
 }
