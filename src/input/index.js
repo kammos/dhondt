ada04d9e79ae47a775ba7994a4a66d860b39c7e4
brainlessbadger <kamil.mosior@onet.pl>
2022-02-05 14:50:02 +0100

    add custom checkbox input

--- src/input/index.js
+++ src/input/index.js
@@ -1,10 +1,11 @@
+import { cnb } from "cnbuilder";
 import camelCase from "lodash/camelCase";
 import startCase from "lodash/startCase";
 import s from "./styles.scss";
 
-export const Input = ({name, className, style, ...rest}) => {
-    return <label className={s.label}>
+export const Input = ({name, type, className, style, ...rest}) => {
+    return <label className={cnb(s.label, s[type])}>
         {startCase(camelCase(name))}:
-        <input name={name} className={s.input} {...rest} />
+        <input name={name} type={type} className={s.input} {...rest} />
     </label>
 }
