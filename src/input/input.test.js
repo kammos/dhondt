2d6d7f4ca669edac8a58aa2e080dc63e9c7ae262
brainlessbadger <kamil.mosior@onet.pl>
2022-02-10 13:20:26 +0100

    fix React dev warning in tests

--- src/input/input.test.js
+++ src/input/input.test.js
@@ -6,25 +6,25 @@ import { Input } from '.';
 
 describe('Input component', () => {
     it('should support text inputs', () => {
-        const ui = render(<Input name="input" value="value" />);
+        const ui = render(<Input name="input" value="value" readOnly/>);
 
         expect(ui.container).toMatchSnapshot();
     });
 
     it('should support number inputs', () => {
-        const ui = render(<Input name="input" type="number" value="100" />);
+        const ui = render(<Input name="input" type="number" value="100" readOnly/>);
 
         expect(ui.container).toMatchSnapshot();
     });
 
     it('should support checkboxes', () => {
-        const ui = render(<Input name="input" type="checkbox" />);
+        const ui = render(<Input name="input" type="checkbox" readOnly/>);
 
         expect(ui.container).toMatchSnapshot();
     });
 
     it('should remove floating point values', () => {
-        const ui = render(<Input name="input" type="number" value="100.1" />);
+        const ui = render(<Input name="input" type="number" value="100.1" readOnly/>);
 
         const input = ui.getByLabelText(/Input/);
         expect(input.value).toBe('');
